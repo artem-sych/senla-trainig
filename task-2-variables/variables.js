bea3f1a820d872bdd1de0a0db4162ef23c2cbb4a
Артем Сыч <Sych.A@ter.net>
2021-03-22 16:12:05 +0300

    test

---
+++ task-2-variables/variables.js
@@ -0,0 +1,17 @@
+/*const userNameFirst;
+const userNameSecond;
+let userAge;
+let userInfo;*/
+
+
+
+
+
+
+
+
+var x = 'string';
+var x = 'string 2';
+console.log(x); // string 2 т.к. 2ое объявление переменной перезаписывает 1ое значение
+
+
