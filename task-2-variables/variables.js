944999ab190a29e1d4477b61761d3f29790919dc
Артем Сыч <Sych.A@ter.net>
2021-03-22 16:16:36 +0300

    разобраться с var

--- task-2-variables/variables.js
+++ task-2-variables/variables.js
@@ -1,17 +1,34 @@
-/*const userNameFirst;
+//Придумать название для переменных, которые описывают (let, const)
+
+const userNameFirst;
 const userNameSecond;
 let userAge;
-let userInfo;*/
+let userInfo;
 
 
+//Что будет в консоле и почему:
 
+// 1
+console.log(test);
+var test = 'string'; // undefined, но почему, непонятно (связано с var)
 
-
-
-
-
+// 2
 var x = 'string';
 var x = 'string 2';
-console.log(x); // string 2 т.к. 2ое объявление переменной перезаписывает 1ое значение
+console.log(x); // string 2, но почему, непонятно (связано с var)
+
+// 3
+console.log(test);
+let test = 'string'; // ошибка, переменная не определена
+
+// 4
+const x = 'string';
+const x = 'string 2';
+console.log(x); // ошибка, нельзя объявлять 2 раза константу с одинаковым названием
+
+// 5
+let num = 12;
+let num = 1;
+console.log(num); // ошибка, нельзя объявлять 2 раза переменную с одинаковым названием
 
 
