5f0c29923745494d6f9566374378d0340abee914
Артем Сыч <Sych.A@ter.net>
2021-03-29 16:32:07 +0300

    task 1 done

--- task-2-array/task-2-array.js
+++ task-2-array/task-2-array.js
@@ -1,9 +1,9 @@
 // 1) Используя функцию, найти последний элемент массива, не изменяя его.
 
 let arr = ["Яблоко", "Апельсин", "Слива", "Банан"];
-/*console.log(arr[arr.length - 1]);*/
+console.log(arr[arr.length - 1]);
 
 let getLastItem = arr.find(function (item, index, array) {
-    return ;
+    return index === arr.length - 1;
 });
 console.log(getLastItem);
