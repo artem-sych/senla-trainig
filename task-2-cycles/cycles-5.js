bfce9a21dc133a072bb197b7660e0c7f209e5dd9
Артем Сыч <Sych.A@ter.net>
2021-03-29 15:32:32 +0300

    done

--- task-2-cycles/cycles-5.js
+++ task-2-cycles/cycles-5.js
@@ -1,6 +1,6 @@
 //В строке "теперь я мастер циклов javascript"
 
-let str = "теперь я мастер циклов javascript";
+let str = "теперь  я мастер циклов javascript";
 let x;
 for (let i = 0; i < str.length; i++) {
     if (i === 0 && i !== ' ') {
