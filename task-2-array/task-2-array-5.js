6f46c9f7e24f6c5ad9984fd381fc1311e53c2fc6
Артем Сыч <Sych.A@ter.net>
2021-03-31 17:00:38 +0300

    3, 4 done

---
+++ task-2-array/task-2-array-5.js
@@ -0,0 +1,15 @@
+// 4) Создать такую функцию, которая принимала бы любое число массивов и удаляла из каждого массива первый элемент, 
+//  а возвращала массив оставшихся значений ([1, 2, 3], ["x", "y", "z"] → [[2, 3], ["y", "z"]])"
+
+let arr1 = [1, 2, 3];
+let arr2 = ["x", "y", "z"];
+let arr = [arr1, arr2];
+
+function delFirstItem(...arr) {
+    arr.forEach(function (item, i, arr) {
+        item.splice(0, 1);
+        console.log(arr);
+    })
+}
+
+delFirstItem(...arr);
